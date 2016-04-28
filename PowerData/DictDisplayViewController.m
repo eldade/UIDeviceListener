435248d5e9bcff27ec3cbcc56b9aca64cd6e29a6
eldoogy <ldoogy@gmail.com>
2016-04-28 13:57:01 -0700

    Restructured the directory tree to make it a bit cleaner.

---
+++ PowerData/DictDisplayViewController.m
@@ -0,0 +1,62 @@
+/*
+ *
+ *    Copyright (C) 2016 Eldad Eilam
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This Program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with PowerData.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#import "DictDisplayViewController.h"
+#import "UIDeviceListener.h"
+
+@interface DictDisplayViewController ()
+
+@end
+
+@implementation DictDisplayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIDeviceListener *listener = [UIDeviceListener sharedUIDeviceListener];
+    
+    [listener startListenerWithNotificationBlock:^(CFDictionaryRef newDict) {
+        NSDictionary *dict = CFBridgingRelease(newDict);
+        self.textView.text = [dict description];
+        [[UIDeviceListener sharedUIDeviceListener] stopListener];
+        [[UIDeviceListener sharedUIDeviceListener] startListenerWithNotificationBlock:^(CFDictionaryRef newDict) {
+            NSDictionary *dict = CFBridgingRelease(newDict);
+            self.textView.text = [dict description];
+        }];
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
