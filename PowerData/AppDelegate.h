e32027cbf5d07a30774a6a22dbaf48ba8c5a3860
Eldad Eilam <eldade@eldads-macbook-pro-8.local>
2016-04-27 23:06:39 -0700

    Initial Commit

---
+++ PowerData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerData
+//
+//  Created by Eldad Eilam on 4/27/16.
+//  Copyright © 2016 Eldad Eilam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PowerData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PowerData
+//
+//  Created by Eldad Eilam on 4/27/16.
+//  Copyright © 2016 Eldad Eilam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
